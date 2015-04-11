f77f2dac59fb69eb35d92f54f34ea5423718c690
nestofsaws <zims@nestofsaws.com>
2015-04-11 12:18:59 -0400

    updating settings.py

--- musicalProgram/settings.py
+++ musicalProgram/settings.py
@@ -62,7 +62,11 @@ WSGI_APPLICATION = 'musicalProgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'musicalProgram.db'),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
