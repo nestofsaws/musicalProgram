42d186d2a09fa5991364dad680d066099a40d6ac
nestofsaws <zims@nestofsaws.com>
2015-04-11 18:06:31 -0400

    updating admin

--- programs/admin.py
+++ programs/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from programs.models import Song, Artist
 
 # Register your models here.
+
+class SongAdmin(admin.ModelAdmin):
+    search_fields = ('title',)
+
+admin.site.register(Song, SongAdmin)
+
+class ArtistAdmin(admin.ModelAdmin):
+    search_fields = ('last_name',)
+
+admin.site.register(Artist, ArtistAdmin)
