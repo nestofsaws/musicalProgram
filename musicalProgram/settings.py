8f34610521ba3af284587ea7783b1dd8f65b0c27
nestofsaws <zims@nestofsaws.com>
2015-04-11 12:24:17 -0400

    updating settings.py

--- musicalProgram/settings.py
+++ musicalProgram/settings.py
@@ -87,4 +87,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'public', 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
+
+MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
