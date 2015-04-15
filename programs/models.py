9737362d875a3a8d7c2148250b74d9fd59ba9a15
Leslie Ray <lesliehealray@gmail.com>
2015-04-15 19:19:06 -0400

    altered models to provide better standardization

--- programs/models.py
+++ programs/models.py
@@ -1,33 +1,28 @@
 from django.db import models
-from django.utils import timezone
 
-# Create your models here.
-class Artist (models.Model):
-    first_name = models.CharField(unique=False, max_length=50)
-    last_name = models.CharField(unique=False, max_length=50)    
-    artistid = models.CharField(unique = True, max_length=12)
-    url = models.CharField(unique=True, max_length=200, null=True)
+
+class Artist(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)    
+    url = models.URLField(null=True, blank=True)
   
     class Meta(object):
         ordering = ('last_name', 'first_name')
 
-    def __unicode__(self):
-        return U'%s %s' %(self.last_name, self.id) 
+    def __str__(self):
+        return '%s %s' %(self.last_name, self.first_name) 
+
 
 class Song(models.Model):
-    title = models.CharField(unique=False, max_length=50)
-    songid = models.CharField(unique = True, max_length=12, default='')
-    key = models.CharField(unique=False, max_length=50, null=True)
-    language = models.CharField(unique=False, max_length=50, null=True)
-    date = models.CharField(unique=False, max_length=50, null=True)
-    instrumentation = models.CharField(unique=False, max_length=50, null=True)
-    description = models.CharField(max_length = 200, default='')
-    composers = models.ManyToManyField('Artist')
+    title = models.CharField(max_length=50)
+    key = models.CharField(max_length=10, null=True, blank=True)
+    language = models.CharField(max_length=50, null=True, blank=True)
+    composed_on = models.IntegerField(max_length=4, null=True, blank=True)
+    performers = models.ManyToManyField('Artist')
 
     class Meta(object):
-        verbose_name_plural = 'Songs'
-        ordering = ('title', 'date')
+        ordering = ('title',)
 
-    def __unicode__(self):
-        return u'%s' % (self.title)
+    def __str__(self):
+        return self.title
           
