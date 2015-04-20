0b4be2bb0caaf710ab987a0fca8019834c6bc27e
nestofsaws <zims@nestofsaws.com>
2015-04-20 14:57:49 -0400

    fixed the issues with the photos!! yay!

--- musicalProgram/urls.py
+++ musicalProgram/urls.py
@@ -1,9 +1,20 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^blog/', include('blog.urls')),
     url(r'^', include('programs.urls')),	
     url(r'^admin/', include(admin.site.urls)),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.STATIC_ROOT,
+        }),
 )
